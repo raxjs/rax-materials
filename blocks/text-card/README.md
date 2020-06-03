888ce728a2e531d4f34e358f95e3caa238558af3
yangfan <18767120422@163.com>
2020-06-03 16:44:08 +0800

    Feat: add blocks (#2)
    * feat: add block
    
    * feat: update review

---
+++ blocks/text-card/README.md
@@ -0,0 +1,5 @@
+# @rax-materials/blocks-text-card
+
+Use for iceworks block template
+![img](https://img.alicdn.com/tfs/TB1WI0xI1L2gK0jSZPhXXahvXXa-345-62.png)
+
---
+++ blocks/text-card/src/index.tsx
@@ -0,0 +1,17 @@
+import { createElement } from 'rax';
+import View from 'rax-view';
+import Text from 'rax-text';
+
+import './index.css';
+
+const MyComponent = (props) => {
+  const { title = "Title", description = "Description" } = props;
+  return (
+    <View className="text-card">
+      <Text className="title">{title}</Text>
+      <Text className="description">{description}</Text>
+    </View>
+  );
+};
+
+export default MyComponent;
