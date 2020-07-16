fa77b30eba5bbd669010f702118ec7d6eb014599
yangfan <18767120422@163.com>
2020-07-16 15:45:58 +0800

    feat: add components

--- scripts/upload.ts
+++ scripts/upload.ts
@@ -1,21 +1,26 @@
 import * as oss from 'ali-oss';
 import * as path from 'path';
+import injectComponents from './injectComponents';
 
 const bucket = 'iceworks';
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 
-oss({
-  bucket,
-  endpoint: 'oss-cn-hangzhou.aliyuncs.com',
-  accessKeyId,
-  accessKeySecret,
-  timeout: '120s',
-})
-  // @ts-ignore
-  .put(
-    path.join('assets', 'materials', 'rax-materials.json'),
-    path.resolve(__dirname, '../build/materials.json')
-  ).then(result => {
-    console.log('materials.json upload success', result);
-  });
+injectComponents().then(() => {
+  oss({
+    bucket,
+    endpoint: 'oss-cn-hangzhou.aliyuncs.com',
+    accessKeyId,
+    accessKeySecret,
+    timeout: '120s',
+  })
+    // @ts-ignore
+    .put(
+      path.join('assets', 'materials', 'rax-materials.json'),
+      path.resolve(__dirname, '../build/materials.json')
+    ).then(result => {
+      console.log('materials.json upload success', result);
+    });
+}).catch((e) => {
+  console.error(e);
+});
