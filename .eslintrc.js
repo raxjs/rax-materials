8983bc4afe9b26268cb65336f8c99df029cdbe91
yangfan <18767120422@163.com>
2020-10-28 21:43:29 +0800

    feat: update spec

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ const { getESLintConfig } = require('@iceworks/spec');
 
 module.exports = getESLintConfig('rax-ts', {
   rules: {
+    '@typescript-eslint/no-require-imports': 'off',
     '@iceworks/best-practices/no-js-in-ts-project': 'off'
   }
 });
