4aaa33441bf18b5eaa40da0a6bbb13d7007dcfc9
yangfan <18767120422@163.com>
2020-12-16 11:12:20 +0800

    feat: update review

--- scripts/upload.ts
+++ scripts/upload.ts
@@ -8,16 +8,14 @@ const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 
 injectComponents()
   .then(() => {
-    oss({
+    return oss({
       bucket,
       endpoint: 'oss-cn-hangzhou.aliyuncs.com',
       accessKeyId,
       accessKeySecret,
       timeout: '120s',
-    })
-      // @ts-ignore
-      .put(path.join('assets', 'materials', 'rax-materials.json'), path.resolve(__dirname, '../build/materials.json'))
-      .then((result) => {
-        console.log('materials.json upload success', result);
-      });
+    }).put(path.join('assets', 'materials', 'rax-materials.json'), path.resolve(__dirname, '../build/materials.json'));
+  })
+  .then((result) => {
+    console.log('materials.json upload success', result);
   });
