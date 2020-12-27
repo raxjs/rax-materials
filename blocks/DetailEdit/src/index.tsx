8e98052c1b88439804bc39b7bc1ae4a2c63500d5
luhc228 <luhengchang228@gmail.com>
2020-12-27 20:37:14 +0800

    feat: DetailEdit material

--- blocks/DetailEdit/src/index.tsx
+++ blocks/DetailEdit/src/index.tsx
@@ -1,13 +1,49 @@
-import { createElement } from 'rax';
+import { createElement, useState } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
+import TextInput from 'rax-textinput';
 
 import './index.css';
 
+const styles = {
+  multiline: {
+    borderWidth: '1rpx',
+    borderColor: '#ddd',
+    flex: 1,
+    fontSize: '26rpx',
+    height: '100rpx',
+    padding: '8rpx',
+    marginBottom: '8rpx',
+  },
+};
+
 const InfoEdit = () => {
+  const [text, setText] = useState();
+
+  const onSubmit = () => {
+    console.log('text', text);
+  };
+
+  const onCancel = () => {
+    console.log('onCancel');
+  };
+
   return (
     <View className="wrap">
-      <Text>Hello World!</Text>
+      <View className="header">
+        <Text className="btn" onClick={onCancel}>取消</Text>
+        <Text className="title">修改账号简介</Text>
+        <Text className="btn submitBtn" onClick={onSubmit}>完成</Text>
+      </View>
+      <View className="textInput">
+        <TextInput
+          multiline
+          style={styles.multiline}
+          value={text}
+          numberOfLines={3}
+          onChangeText={(value) => setText(value)}
+        />
+      </View>
     </View>
   );
 };
