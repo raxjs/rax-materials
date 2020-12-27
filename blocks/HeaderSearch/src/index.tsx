7dff3626ee038cd66572855b4c1affdad525a4fe
luhc228 <luhengchang228@gmail.com>
2020-12-27 20:07:35 +0800

    feat: update UserDetailForm material

--- blocks/HeaderSearch/src/index.tsx
+++ blocks/HeaderSearch/src/index.tsx
@@ -1,13 +1,20 @@
-import { createElement } from 'rax';
+import { createElement, useState } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
+import { Search } from '@alifd/meet';
 
 import './index.css';
 
 const HeaderSearch = () => {
+  const [searchValue, setSearchValue] = useState('淘小橙');
+
+  const onSearchChange = (value) => {
+    setSearchValue(value);
+  };
   return (
     <View className="wrap">
-      <Text>Hello World!</Text>
+      <Search size="medium" value={searchValue} hasClear hasCancel onChange={onSearchChange} />
+      <Text>搜索发现</Text>
     </View>
   );
 };
--- blocks/UserDetailForm/src/index.tsx
+++ blocks/UserDetailForm/src/index.tsx
@@ -2,6 +2,8 @@ import { createElement } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
 import ScrollView from 'rax-scrollview';
+import Picture from 'rax-picture';
+
 import './index.css';
 
 const DetailForm = () => {
@@ -9,24 +11,35 @@ const DetailForm = () => {
     <ScrollView
       className="page"
     >
-      <View className="section-title">
-        <Text style={{
-          fontSize: '32rpx',
-          color: '#111111',
-          fontWeight: 'bold',
-        }}
-        >
-          基本信息
-        </Text>
-      </View>
+      <View className="section-title">基本信息</View>
       <View className="section-container">
+        <View className="section-item" style={{ height: '107.5rpx' }}>
+          <View className="section-item-left">
+            <Text className="section-item-left-text">实人认证</Text>
+          </View>
+          <View className="section-item-right">
+            <Text className="section-item-right-text" numberOfLines={1}>
+              已认证成功
+            </Text>
+          </View>
+        </View>
         <View className="section-item" style={{ height: '107.5rpx' }}>
           <View className="section-item-left">
             <Text className="section-item-left-text">身份类型</Text>
           </View>
           <View className="section-item-right">
             <Text className="section-item-right-text" numberOfLines={1}>
-              111
+              美妆主播
+            </Text>
+          </View>
+        </View>
+        <View className="section-item" style={{ height: '107.5rpx' }}>
+          <View className="section-item-left">
+            <Text className="section-item-left-text">细分类型</Text>
+          </View>
+          <View className="section-item-right">
+            <Text className="section-item-right-text" numberOfLines={1}>
+              视频教程类美妆
             </Text>
           </View>
         </View>
@@ -36,47 +49,63 @@ const DetailForm = () => {
           </View>
           <View className="section-item-right">
             <Text className="section-item-right-text" numberOfLines={1}>
-              111
+              知名美妆主播
             </Text>
           </View>
         </View>
-        {/* <View className="condition-line" /> */}
+        <View className="section-item">
+          <View className="section-item-left">
+            <Text className="section-item-left-text">职业证明材料</Text>
+          </View>
+          <View className="section-item-img-list">
+            {/* <Text className="section-item-right-text" numberOfLines={1}>
+              知名美妆主播
+            </Text> */}
+            <Picture className="pic" source={{ uri: 'https://img.alicdn.com/imgextra/i3/O1CN019K6xhr1Dy78A13l40_!!6000000000284-2-tps-152-152.png' }} />
+            <Picture className="pic" source={{ uri: 'https://img.alicdn.com/imgextra/i3/O1CN010MIGML1ZCrou55eqO_!!6000000003159-2-tps-152-152.png' }} />
+          </View>
+        </View>
       </View>
-      <View className="section-title">
-        <Text style={{
-          fontSize: '32rpx',
-          color: '#111111',
-          fontWeight: 'bold',
-        }}
-        >
-          联系方式
-        </Text>
-      </View>
+      <View className="section-title">运营平台信息</View>
       <View className="section-container">
         <View className="section-item" style={{ height: '107.5rpx' }}>
           <View className="section-item-left">
-            <Text className="section-item-left-text">联系人姓名</Text>
+            <Text className="section-item-left-text">运营平台名称</Text>
           </View>
           <View className="section-item-right">
-            <Text className="section-item-right-text" numberOfLines={1}>111</Text>
+            <Text className="section-item-right-text" numberOfLines={1}>哔哩哔哩</Text>
           </View>
         </View>
-        {/* <View className="condition-line" /> */}
         <View className="section-item" style={{ height: '107.5rpx' }}>
           <View className="section-item-left">
-            <Text className="section-item-left-text" >联系人电话</Text>
+            <Text className="section-item-left-text" >平台主页链接</Text>
           </View>
           <View className="section-item-right">
-            <Text className="section-item-right-text" numberOfLines={1}>11</Text>
+            <Text className="section-item-right-text" numberOfLines={1}>https://m.taobao.com</Text>
           </View>
         </View>
-        {/* <View className="condition-line" /> */}
         <View className="section-item" style={{ height: '107.5rpx' }}>
           <View className="section-item-left">
-            <Text className="section-item-left-text">联系人微信</Text>
+            <Text className="section-item-left-text">擅长内容方向</Text>
           </View>
           <View className="section-item-right">
-            <Text className="section-item-right-text" numberOfLines={1}>111</Text>
+            <Text className="section-item-right-text" numberOfLines={1}>海鲜类美食分享</Text>
+          </View>
+        </View>
+        <View className="section-item" style={{ height: '107.5rpx' }}>
+          <View className="section-item-left">
+            <Text className="section-item-left-text">平台粉丝数量</Text>
+          </View>
+          <View className="section-item-right">
+            <Text className="section-item-right-text" numberOfLines={1}>23,468,693</Text>
+          </View>
+        </View>
+        <View className="section-item" style={{ height: '107.5rpx' }}>
+          <View className="section-item-left">
+            <Text className="section-item-left-text">平台作品数量</Text>
+          </View>
+          <View className="section-item-right">
+            <Text className="section-item-right-text" numberOfLines={1}>456</Text>
           </View>
         </View>
       </View>
