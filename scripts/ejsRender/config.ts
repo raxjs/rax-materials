e188b48887d50eb2a77eb4d675a55a2b0145ac4f
yangfan <18767120422@163.com>
2021-07-16 20:08:30 +0800

    feat: add preview tooltip

--- scripts/ejsRender/config.ts
+++ scripts/ejsRender/config.ts
@@ -9,6 +9,11 @@ const ejsData = {
   pha: false,
 };
 
+const previewData = [
+  { name: 'Web', message: '建议使用手机浏览器扫码', output: 'web/index.html' },
+  // { name: 'Weex', message: '建议使用手机淘宝扫码', output: 'weex/index.js' },
+];
+
 // Add preview to ejs scaffolds template.
 // Build or dev scaffolds project you should see blew config first.
 export default {
@@ -19,9 +24,10 @@ export default {
     'app-js': {
       target: path.join(ROOT, 'scaffolds/app-js/'), // target template path
       data: ejsData, // ejs render data
+      preview: previewData, // preview url for qrcode
     },
-    'app-ts': { target: path.join(ROOT, 'scaffolds/app-ts/'), data: ejsData },
-    'app-midway-faas': { target: path.join(ROOT, 'scaffolds/app-midway-faas/'), data: ejsData },
-    'app-midway-ssr': { target: path.join(ROOT, 'scaffolds/app-midway-faas/'), data: ejsData },
+    'app-ts': { target: path.join(ROOT, 'scaffolds/app-ts/'), data: ejsData, preview: previewData },
+    'app-midway-faas': { target: path.join(ROOT, 'scaffolds/app-midway-faas/'), data: ejsData, preview: previewData },
+    'app-midway-ssr': { target: path.join(ROOT, 'scaffolds/app-midway-faas/'), data: ejsData, preview: previewData },
   },
 };
--- scripts/ejsRender/prePublish.ts
+++ scripts/ejsRender/prePublish.ts
@@ -13,6 +13,7 @@ export default function (directory: string) {
   const index = keys.map((key) => targets[key].target).findIndex((target) => path.join(directory, './').indexOf(target) === 0);
   if (index !== -1) {
     const key = keys[index];
+    const target = targets[key];
     const targetDir = path.join(tmpDir, key);
 
     spawnSync(
@@ -23,6 +24,33 @@ export default function (directory: string) {
         cwd: targetDir,
       },
     );
+
+    // Update temp config file to append scripts for tooltip(show qrcode and some information)
+    try {
+      const appJsonPath = path.join(targetDir, 'src/app.json');
+      const buildJsonPath = path.join(targetDir, 'build.json');
+
+      const appJson = fs.readJsonSync(appJsonPath);
+      const buildJson = fs.readJsonSync(buildJsonPath);
+
+      // Update app.json
+      if (!appJson.scripts) {
+        appJson.scripts = [];
+      }
+      const { name, version } = fs.readJsonSync(path.join(targetDir, 'package.json'));
+      appJson.scripts.push(`<script>window.PREVIEW_URLS = [${target.preview.map((item) => `{ name: '${item.name}', message: '${item.message}', url: 'https://unpkg.com/${name}/@${version}/build/${item.output}' },`)}];</script>`);
+      appJson.scripts.push('<script src="https://dev.g.alicdn.com/appworks/rax-scaffolds-preview-fab/0.0.1/index.js"></script>');
+
+      // Update build.json
+      buildJson.publicPath = './';
+
+      // ReWrite
+      fs.writeFileSync(appJsonPath, JSON.stringify(appJson, null, 2));
+      fs.writeFileSync(buildJsonPath, JSON.stringify(buildJson, null, 2));
+    } catch (e) {
+      console.log('Process config file failed: ', e);
+    }
+
     spawnSync(
       'npm',
       ['run', 'build'],
