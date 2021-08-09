7905489fcbffd73e59096e4196bde2dd400bbb5a
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-08-09 15:53:38 +0800

    fix: lint error

--- scaffolds/app-midway-miniapp/src/pages/Home/index.tsx
+++ scaffolds/app-midway-miniapp/src/pages/Home/index.tsx
@@ -3,16 +3,16 @@ import useAsyncEffect from 'rax-use-async-effect';
 import View from 'rax-view';
 import Text from 'rax-text';
 import styles from './index.module.css';
-import Logo from '../../components/Logo';
+import Logo from '@/components/Logo';
 import { hello, getOpenId } from '@/cloud/function';
 
 export default function Home() {
-  const [message, setMessage] = useState('');
-  const [openId, setOpenId] = useState('');
+  const [currentMessage, setMessage] = useState('');
+  const [currentOpenId, setOpenId] = useState('');
 
   useAsyncEffect(async () => {
-    const message = await hello('Rax', 'Midway.js');
-    setMessage(message);
+    const newMessage = await hello('Rax', 'Midway.js');
+    setMessage(newMessage);
 
     const { openId } = await getOpenId();
     setOpenId(`${openId!.substring(0, 3) }***************${ openId!.substring(14)}`);
@@ -22,8 +22,8 @@ export default function Home() {
     <View className={styles.homeContainer}>
       <Logo uri="//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png" />
       <Text className={styles.homeTitle}>Welcome to Your Rax App</Text>
-      <Text className={styles.homeInfo}>Message: {message}</Text>
-      <Text className={styles.homeInfo}>OpenId: {openId}</Text>
+      <Text className={styles.homeInfo}>Message: {currentMessage}</Text>
+      <Text className={styles.homeInfo}>OpenId: {currentOpenId}</Text>
     </View>
   );
 }
