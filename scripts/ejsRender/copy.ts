a2b6abc56060705d88ad21d00f844fb27fe8000f
yangfan <18767120422@163.com>
2021-08-10 20:26:04 +0800

    feat: update

--- scripts/ejsRender/copy.ts
+++ scripts/ejsRender/copy.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
-const ignores = ['node_modules', '.rax', '.ice', 'build', 'lib'];
+const ignores = ['node_modules/'];
 
 export default function copy(from: string, to: string) {
   const reg = new RegExp(ignores.map((ignore) => path.join(from, ignore)).join('|'));
